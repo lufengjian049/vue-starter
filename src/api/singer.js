203073ff449ca7cedf84bba9a185834fd4031fdc
lufj <lufengjian0409@163.com>
2018-04-13 00:30:50 +0800

    singer

--- src/api/singer.js
+++ src/api/singer.js
@@ -19,3 +19,19 @@ export function getSingerList() {
     platform: 'yqq'
   }, options)
 }
+
+export function getSingerDetail(singerId) {
+  const url = 'https://c.y.qq.com/v8/fcg-bin/fcg_v8_singer_track_cp.fcg'
+
+  return jsonp(url, {
+    ...commonParams,
+    hostUin: 0,
+    needNewCode: 0,
+    platform: 'yqq',
+    order: 'listen',
+    begin: 0,
+    num: 80,
+    songstatus: 1,
+    singermid: singerId
+  }, options)
+}
